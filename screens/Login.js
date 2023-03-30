55ce14df3a6a00bd1c756dcdc9aad8e55badbf99
pranavi-m <108517788+pranavi-m@users.noreply.github.com>
2023-03-30 17:17:50 +0530

    Add files via upload

---
+++ screens/Login.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  ImageBackground,
+  Image,
+  Alert,
+  ToastAndroid,
+  KeyboardAvoidingView
+} from "react-native";
+import firebase from "firebase";
+
+const bgImage = require("../assets/background2.png");
+const appIcon = require("../assets/appIcon.png");
+const appName = require("../assets/appName.png");
+
+export default class LoginScreen extends Component{
+  
+    constructor(props){
+        super(props)
+        this.state={
+            email:"",
+            password:""
+        }
+    }
+
+    handleLogin=(email,password)=>{
+    firebase.auth().signInWithEmailAndPassword(email,password).then(()=>{
+        this.props.navigation.navigate("BottomTab")
+    }) 
+    .catch((error)=>{Alert.alert(error.message)})
+    }
+
+    render(){
+        const {email,password}=this.state
+    return(
+        <KeyboardAvoidingView behavior="padding">
+        <ImageBackground source={bgImage}>
+        <View>
+            <Image source = {appIcon}/>
+            <Image source = {appName}/>
+        </View>
+        <View>
+            <TextInput
+            onChangeText={(text)=>{this.setState({email:text})}}
+            placeholder={"Email"}
+            placeholderTextColor={"white"}
+            autoFocus
+            />
+            <TextInput
+            onChangeText={(text)=>{this.setState({password:text})}}
+            placeholder={"Password"}
+            placeholderTextColor={"white"}
+            secureTextEntry
+            />
+            <TouchableOpacity 
+            onPress = {()=>this.handleLogin(email,password)}>
+                <Text>
+                    Login
+                </Text>
+            </TouchableOpacity>
+        </View>
+        </ImageBackground>
+        </KeyboardAvoidingView>
+    )
+    }
+    
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1, backgroundColor: "#FFFFFF" }, 
+    bgImage: { flex: 1, resizeMode: "cover", justifyContent: "center" }, upperContainer: { flex: 0.5, justifyContent: "center", alignItems: "center" }, appIcon: { width: 280, height: 280, resizeMode: "contain", marginTop: 80 }, appName: { width: 130, height: 130, resizeMode: "contain" }, lowerContainer: { flex: 0.5, alignItems: "center" }, textinput: { width: "75%", height: 55, padding: 10, borderColor: "#FFFFFF", borderWidth: 4, borderRadius: 10, fontSize: 18, color: "#FFFFFF", fontFamily: "Rajdhani_600SemiBold", backgroundColor: "#5653D4" }, button: { width: "43%", height: 55, justifyContent: "center", alignItems: "center", backgroundColor: "#F48D20", borderRadius: 15 }, 
+    buttonText: { fontSize: 24, color: "#FFFFFF", fontFamily: "Rajdhani_600SemiBold"}
+
+})
